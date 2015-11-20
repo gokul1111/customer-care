e1b484738b9fd91c9493d2da56eda53cf79ce1f3
kumuda <incredible.kumda@gmail.com>
2015-11-20 12:22:03 +0530

    removed unwanted dependencies

---
+++ js/app/CustomerService.js
@@ -0,0 +1,18 @@
+angular.module('Auth').factory('ApiService', function($http, $q) {
+	return {
+		getQueue: function(){
+			var deferred = $q.defer();
+			$http({
+				method: 'GET',
+				url: 'queue.json'
+			}).then(function(response) {
+					console.log(response);
+                    deferred.resolve(response.data);
+
+				}, function(){
+
+				});
+			return deferred.promise;
+		},
+	}
+});
